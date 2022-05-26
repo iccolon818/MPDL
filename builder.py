d7f80f61b7ad6575cb2a9e6eaeeb926f725c2a0e
iccolon818 <iccolon818@gmail.com>
2022-05-26 13:27:02 -0400

    Glade file path is relative to script path

--- builder.py
+++ builder.py
@@ -1,9 +1,11 @@
 from gi import require_version
 require_version('Gtk', '3.0')
 from gi.repository.Gtk import Builder
+import os
 
 builder = Builder()
-builder.add_from_file("MPDL.glade")
+dir = os.path.dirname(__file__)
+builder.add_from_file(dir + "/MPDL.glade")
 
 def get_object(obj):
     return builder.get_object(obj)
